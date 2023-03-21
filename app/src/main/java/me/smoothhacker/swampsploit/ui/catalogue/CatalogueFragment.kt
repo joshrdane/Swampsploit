47880a3068cc492bf6f873d7800fe8dd88c67dcd
Scott Lagler <laglerscott@gmail.com>
2023-03-21 16:19:07 -0400

    feat: UI updates (#4)
    * fixes to exploit page control flow
    
    * pushing to work on my other device
    
    ---------
    
    Co-authored-by: Gabriella Neris <58517642+gabriellaneris@users.noreply.github.com>

--- app/src/main/java/me/smoothhacker/swampsploit/ui/catalogue/CatalogueFragment.kt
+++ app/src/main/java/me/smoothhacker/swampsploit/ui/catalogue/CatalogueFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import me.smoothhacker.swampsploit.databinding.FragmentGalleryBinding
+import me.smoothhacker.swampsploit.databinding.FragmentExploitGalleryBinding
 import me.smoothhacker.swampsploit.ui.exploit.ExploitActivity
 
 class CatalogueFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentExploitGalleryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +26,7 @@ class CatalogueFragment : Fragment() {
         val catalogueViewModel =
             ViewModelProvider(this)[CatalogueViewModel::class.java]
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentExploitGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         binding.ProFTPDButton.setOnClickListener {
--- app/src/main/java/me/smoothhacker/swampsploit/ui/reports/ReportsViewModel.kt
+++ app/src/main/java/me/smoothhacker/swampsploit/ui/reports/ReportsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class ReportsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is reports Fragment"
     }
     val text: LiveData<String> = _text
 }
