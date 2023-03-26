715473984fcf44be3b256b4de3c0e6121f9c6cef
Scott Lagler <laglerscott@gmail.com>
2023-03-26 15:36:21 -0400

    feat: add first unit test for ExploitContext save/load

--- app/src/test/java/me/smoothhacker/swampsploit/ExampleUnitTest.kt
+++ app/src/test/java/me/smoothhacker/swampsploit/ExampleUnitTest.kt
@@ -1,17 +1,25 @@
 package me.smoothhacker.swampsploit
 
+import me.smoothhacker.swampsploit.utils.ExploitContext
+
 import org.junit.Test
-
 import org.junit.Assert.*
+import java.io.File
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class ExploitContextTests {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testSavingToDisk() {
+        val dataFile = File("testExploitContext")
+        val ctx = ExploitContext(dataFile)
+        ctx.setPort(2222)
+        ctx.setTimeout(1000)
+        ctx.addHost("192.168.1.255")
+
+        ctx.save()
+
+        val newCtx = ExploitContext(dataFile)
+        assertEquals(newCtx.getPort(), ctx.getPort())
+        assertEquals(newCtx.getTimeout(), ctx.getTimeout())
+        assertEquals(newCtx.getHost(0), ctx.getHost(0))
     }
 }
